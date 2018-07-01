bc9fd9e2f7eb7a927c3b8881e4119bb2bfcd4bc1
Travis Sluka <travissluka@gmail.com>
2018-06-30 21:24:21 -0400

    beginning inflation tests

--- test/getTestData.sh
+++ test/getTestData.sh
@@ -3,3 +3,5 @@ mkdir -p DATA
 cd DATA
 wget https://www.dropbox.com/s/4m5bpvb53vtsvlp/mom6.360x270.10mem.tgz
 tar -xaf mom6.360x270.10mem.tgz
+wget https://www.dropbox.com/s/ei7k0jt9vobgt1o/simple3d.tgz
+tar -xaf simple3d.tgz
